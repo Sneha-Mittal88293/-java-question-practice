08bc57fa2e9e40260733723335c31fd9b898ba13
sneha <snehamittal88293@gmail.com>
2022-02-24 11:55:12 +0530

    sum of n natural number

---
+++ Number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Number{
+      public static void main(String args[]){
+          //for (int i= 0 ; i<10 ;i++)
+           Scanner sc = new Scanner(System.in);
+           int n = sc.nextInt();
+            int sum = 0;
+           for( int i=0 ; i<n ;i++){
+                sum = sum + i;
+
+           }
+           System.out.print(sum);
+        
+           
+         
+      }
+  }
