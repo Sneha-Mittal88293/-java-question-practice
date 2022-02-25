b51ecab0e6cfda0fde499b0a4d0f0c3c55922e75
sneha <snehamittal88293@gmail.com>
2022-02-25 19:12:11 +0530

    solid rhombus

--- Invertedhalfpyramid.java
+++ Invertedhalfpyramid.java
@@ -4,15 +4,16 @@ public class Invertedhalfpyramid{
         Scanner sc =new Scanner(System.in);
        
        int n = sc.nextInt();
-       int s=1;
-      for ( int i=0 ; i<n ;i++){
-          for(int j=0; j<i ; j++){
-              System.out.print(s + " ");
-                 s++;
-          }
-          System.out.print("\n");
-      }
        
+       for(int i = 0 ; i<n ; i++){
+           for(int j =n ; j>=i ; j--){
+               System.out.print(" ");
+           }
+           for(int j = n ; j>0;j-- ){
+               System.out.print("*");
+           }
+           System.out.print("\n");
+       }
 
        
        
