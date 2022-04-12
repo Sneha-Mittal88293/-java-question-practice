715f21e22d74a7bd51720735a2969352b887eaee
sneha <snehamittal88293@gmail.com>
2022-04-12 16:37:56 +0530

    Subsequences

---
+++ Subsequence.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Subsequence{
+    public static void subseq(String str , int idx , String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+
+        //to be
+        subseq(str , idx+1,newString + currChar); 
+
+        //not to be
+        subseq(str , idx+1 ,newString); 
+
+    }
+    public static void main(String args[]){
+        String str = "abc";
+        subseq(str ,0,"") ;
+    }
+}
