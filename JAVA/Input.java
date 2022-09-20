a3281958f8bbad5b911886f25eaf626c93c4b23c
sneha <snehamittal88293@gmail.com>
2022-09-20 18:00:31 +0530

    take input

---
+++ JAVA/Input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        // .next take only one word
+         name = sc.next();
+          System.out.print(name);
+        // .nextline take whole sentence
+         name = sc.nextLine();
+           System.out.print(name);
+    }
+}
