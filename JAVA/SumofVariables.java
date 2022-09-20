650d82dae42cdbf2fdf7074663da21d9ee342633
sneha <snehamittal88293@gmail.com>
2022-09-20 18:09:20 +0530

    sum of two variables

---
+++ JAVA/SumofVariables.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class SumofVariables{
+    public static void main(String args[]){
+        int a , b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        int sum = a+b;
+        System.out.print("sum of two variables is " + sum);
+    }
+}
