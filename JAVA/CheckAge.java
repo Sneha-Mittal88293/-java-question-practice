7d36b55f41365c737703acdbfad40137c639dce3
sneha <snehamittal88293@gmail.com>
2022-09-21 18:55:24 +0530

    example of conditional statement

---
+++ JAVA/CheckAge.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class CheckAge{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>18){
+            System.out.print("Adult");
+        }
+        else{
+            System.out.print("Not adult");
+        }
+    }
+}
