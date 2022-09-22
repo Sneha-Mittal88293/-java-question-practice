e963fbff5b56888cdeccb3ff88e6354a01129571
sneha <snehamittal88293@gmail.com>
2022-09-22 11:22:09 +0530

    check a prime number

---
+++ JAVA/Loops/CheckPrime.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class CheckPrime{
+    public static void  main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+       boolean isPrime= true;
+       for( int i=2 ; i<=n/2 ; i++){
+        if(n%i == 0){
+            isPrime = false;
+            break;
+        }
+       }
+
+       if(isPrime){
+        if(n==1){
+            System.out.print("neither prime nor composite");
+        }
+        else {
+            System.out.print("Prime Number");
+        }
+       }
+       else{
+        System.out.print("Not a Prime Number");
+       }
+
+    }
+}
