ed43a72f3726c5491b4098becc681d2753f27664
sneha <snehamittal88293@gmail.com>
2022-09-27 19:34:12 +0530

    infinitr loop

---
+++ JAVA/Loops/Infinity.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Infinity{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+         
+         int i = -1;
+        do{
+            System.out.print("infinite");
+            i--;
+        }while(i<n);
+        
+    }
+}
