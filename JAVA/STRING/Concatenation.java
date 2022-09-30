73f172c16b9e89536e9c2b6a114409b57c9c3725
sneha <snehamittal88293@gmail.com>
2022-09-30 17:54:46 +0530

    concatenate two string

--- JAVA/STRING/Concatenation.java
+++ JAVA/STRING/Concatenation.java
@@ -8,5 +8,18 @@ class Concatenation{
         String second = sc.nextLine();
 
         System.out.print(first+second);    //concatenation
+        System.out.print(second.length());   //.length
+
+
+     // .charAt(); use for print alphabet
+        for(int i = 0 ; i<second.length() ; i++){
+           System.out.print(second.charAt(i));
+        }
+        
+
+        //comparise -- .compateTo()
+        System.out.print(first.compareTo(second));
+
+
     }
 }
