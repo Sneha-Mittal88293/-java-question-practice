9a3f3ff699c380f921de67f303da9e84055e2ffe
sneha <snehamittal88293@gmail.com>
2022-09-30 19:06:13 +0530

    string builder

---
+++ JAVA/STRING/Stri.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Stri{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder(sc.nextLine());
+        System.out.println(sb);
+        System.out.print(sb.charAt(0));
+    }
+}
