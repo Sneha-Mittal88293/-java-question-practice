618e939cfc7448e6824da19d4896e535152c4df2
sneha <snehamittal88293@gmail.com>
2022-09-30 17:38:00 +0530

    concatenate two string

---
+++ JAVA/STRING/Concatenation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Concatenation{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        String first = sc.nextLine();
+        String second = sc.nextLine();
+
+        System.out.print(first+second);    //concatenation
+    }
+}
