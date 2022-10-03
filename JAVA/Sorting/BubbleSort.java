e42350ea3c23353f5ba0b4266891e6ef817b8c84
sneha <snehamittal88293@gmail.com>
2022-10-03 18:07:17 +0530

    bubblesort

---
+++ JAVA/Sorting/BubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class BubbleSort{
+    public static void printArray(int arr[]){
+        for(int i = 0 ; i<arr.length; i++){
+             System.out.print(arr[i]+ " ");
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int []arr = new int[size];
+
+        for(int i  = 0 ; i<size ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0 ; i<size-1 ; i++){
+            for(int j = 0 ; j<size-i-1 ; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j] ;
+                    arr[j] = arr[j+1] ;
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+
+    }
+}
