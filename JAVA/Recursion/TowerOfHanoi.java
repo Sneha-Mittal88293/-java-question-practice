92fb9b6087f37c5a1f387aaa98c958839842c258
sneha <snehamittal88293@gmail.com>
2022-10-07 15:31:29 +0530

    tower of hanoi

--- JAVA/Recursion/TowerOfHanoi.java
+++ JAVA/Recursion/TowerOfHanoi.java
@@ -1,8 +1,19 @@
 import java.util.*;
 
 class TowerOfHanoi{
+    public static void towerOfHanoi(int n , String src , String helper , String dest){
+            if(n == 1){ 
+                System.out.println("transfer disc"+ n +"from" + src+ "to" +dest);
+                return;
+
+            }
+            towerOfHanoi(n-1 , src , dest , helper);
+            System.out.println("transfer disc"+ n +"from" + src+ "to" +dest);
+            towerOfHanoi(n-1 , helper , src , dest);
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
+        int n = sc.nextInt();
+        towerOfHanoi(n ,"S","H","D");
     }
 }
