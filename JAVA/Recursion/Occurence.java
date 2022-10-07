01917252c332f2ea70f5051369336b66e60d0f93
sneha <snehamittal88293@gmail.com>
2022-10-07 16:22:56 +0530

    firest and last occurence

--- JAVA/Recursion/Occurence.java
+++ JAVA/Recursion/Occurence.java
@@ -1,10 +1,34 @@
 
 import java.util.*;
+
 class Occurence{
-  public static void main(String args[]){
+     public static int first = -1;
+     public static int last = -1;
+    public static void find(char choice ,String str , int idx){
+        if(idx == str.length()){
+            System.out.print(first +" " + last);
+            return;
+        }
+        
+        char currentChar = str.charAt(idx);
+        if(choice == currentChar){
+            if(first == -1){
+                first = idx;
+            }
+            else{
+                last = idx;
+            }
+
+        }
+        find(choice , str , idx+1);
+        }
+    public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     String str = sc.nextLine();
 
+
     
+    find('a' , str , 0);
+
   }
 }
