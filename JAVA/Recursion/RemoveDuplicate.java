9bd9662343d5d3cdb28f90ebd7a6b8303a8ddea7
sneha <snehamittal88293@gmail.com>
2022-10-10 18:35:48 +0530

    remove duplicate element

--- JAVA/Recursion/RemoveDuplicate.java
+++ JAVA/Recursion/RemoveDuplicate.java
@@ -1,8 +1,30 @@
 import java.util.*;
 
 class RemoveDuplicate{
+    public static boolean[] map = new boolean[26];
+    
+    public static void removeduplicate(String str , int idx ,String newString){
+        if(str.length() == idx){
+            System.out.print(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a'] == true){
+            removeduplicate(str , idx+1 , newString);
+        }
+        else{
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeduplicate(str , idx+1 , newString);
+        }
+
+
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
+        String str = sc.nextLine();
+        removeduplicate(str , 0 , "");
+
     }
 }
