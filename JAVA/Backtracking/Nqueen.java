aed7248c5e5ef326b532dc90fc7bd3c624a3d368
sneha <snehamittal88293@gmail.com>
2022-10-17 18:21:52 +0530

    n-quuen

---
+++ JAVA/Backtracking/Nqueen.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Nqueen{
+    public List<List<String>> solveQueens(int n){
+        List<List<String>> = new ArrayList<>();
+        char[][] board = new board[n][n];
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        solveQueens(n);
+    }
+}
