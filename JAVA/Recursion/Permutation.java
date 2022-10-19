28afd2e4d8612c1aafbb12adaf53daa700c1eab7
sneha <snehamittal88293@gmail.com>
2022-10-19 18:48:48 +0530

    space add

--- JAVA/Recursion/Permutation.java
+++ JAVA/Recursion/Permutation.java
@@ -5,6 +5,7 @@ class Permutation{
         System.out.println(newString);
         return;
        }
+       
         for(int i = 0 ; i<str.length() ; i++){
             char currChar = str.charAt(i);
             String newStr = str.substring(0 , i) + str.substring(i+1);
