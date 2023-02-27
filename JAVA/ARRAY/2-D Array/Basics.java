709a984fa0887e39a966a2dff173a2d0ca026a8b
sneha <snehamittal88293@gmail.com>
2023-02-27 11:22:00 +0530

    create 2-d array

---
+++ JAVA/ARRAY/2-D Array/Basics.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Basics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int [][] arr = new int[3][2];
+
+        for(int i = 0 ; i<3 ; i++){
+            for(int j =0 ; j<2 ; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0 ; i<3 ; i++){
+            for(int j =0 ; j<2 ; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
