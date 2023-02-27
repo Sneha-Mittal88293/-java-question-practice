7a77126721bd36f89310a31de169e7d8b1dd12a8
sneha <snehamittal88293@gmail.com>
2023-02-27 11:49:44 +0530

    create and search key in 2-d array

--- JAVA/ARRAY/2-D Array/Basics.java
+++ JAVA/ARRAY/2-D Array/Basics.java
@@ -1,6 +1,19 @@
 import java.util.*;
 
 class Basics{
+    public static boolean Search(int arr[][] ,int key){
+        for(int i = 0 ; i<arr.length ; i++){
+            for(int j =0 ; j<arr[0].length ; j++){
+                if(arr[i][j] == key){
+                    System.out.print("found at" +i +" , "+j);
+                    return true;
+                }
+            }
+        }
+        System.out.print("not found");
+        return false;
+
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int [][] arr = new int[3][2];
@@ -16,6 +29,7 @@ class Basics{
             }
             System.out.println();
         }
+        Search(arr ,11);
 
     }
 }
