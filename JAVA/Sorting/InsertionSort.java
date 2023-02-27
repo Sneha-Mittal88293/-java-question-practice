cff84e7ea8d2ddf4344fe064d2c406e04f0e8d8f
sneha <snehamittal88293@gmail.com>
2023-02-27 09:23:30 +0530

    add sorting programs

--- JAVA/Sorting/InsertionSort.java
+++ JAVA/Sorting/InsertionSort.java
@@ -9,13 +9,10 @@ class InsertionSort{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
             int size = sc.nextInt();
-
             int []arr = new int[size];
-
             for(int i = 0 ; i<size ; i++){
                 arr[i] = sc.nextInt();
             }
-
             for(int i = 0 ; i<size ; i++){
                 int current = arr[i];
                 int j = i-1;
@@ -23,12 +20,8 @@ class InsertionSort{
                    arr[j+1] = arr[j];
                    j--;
                 }
-
                 arr[j+1] = current;
             }
-
-            
-            
             printArray(arr);
         }
     }
